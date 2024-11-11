76bb32ad6b98dd8971b69824878da8cc11514166
todok0317 <todok0317@naver.com>
2024-11-11 19:36:27 +0900

    1_7

--- src/GameView.java
+++ src/GameView.java
@@ -31,7 +31,7 @@ public class GameView extends JPanel {
 	
 	private void initializeLevelSelectButton(GameFrame frame) {
 		levelSelectButton = new JButton("Back");
-		levelSelectButton.setBounds(10, 10, 100, 40); // 위치와 크기 설정
+		levelSelectButton.setBounds(30, 30, 80, 40); // 위치와 크기 설정
 		levelSelectButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -103,15 +103,7 @@ public class GameView extends JPanel {
 		g.setFont(new Font("Arial", Font.BOLD, 24));
 		int panelWidth = getWidth();
 		
-		//g.drawString("time left: " + gameModel.getTimeLeft(), 900, 600);
-		//g.drawString("score: " + gameModel.getScore(), 900, 650);
-	
-		String timeText = "time left: " + gameModel.getTimeLeft();
-		int timeTextWidth = g.getFontMetrics().stringWidth(timeText);
-		g.drawString(timeText, (panelWidth - timeTextWidth) / 2, 40);
-		
-		String scoreText = "score: " + gameModel.getScore();
-		int scoreTextWidth = g.getFontMetrics().stringWidth(scoreText);
-		g.drawString(scoreText, panelWidth - scoreTextWidth - 20, getHeight() - 20);
+		g.drawString("time left: " + gameModel.getTimeLeft(), 900, 600);
+		g.drawString("score: " + gameModel.getScore(), 900, 650);
 	}
 }
