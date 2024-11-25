f098ec312957eb04212247089b1697308d56c9ad
todok0317 <todok0317@naver.com>
2024-11-25 18:09:15 +0900

    4_3 사진추가 아이템 추가

--- src/LevelManager.java
+++ src/LevelManager.java
@@ -25,11 +25,16 @@ public class LevelManager {
 	private LevelData createDataLevel1() {
 		// 레벨1 데이터
 		List<Item> itemTemplates = new ArrayList<>();
-		itemTemplates.add(new Item("배달음식 용기", "플라스틱", "images/plastic_bowl.png"));
+
 		itemTemplates.add(new Item("빈 페트병", "플라스틱", "images/petbottle_black.png"));
+		itemTemplates.add(new Item("물티슈 뚜껑", "플라스틱", "images/plastic_water.png"));
+		itemTemplates.add(new Item("배달음식 용기", "플라스틱", "images/plastic_squre.png"));
 		itemTemplates.add(new Item("콜라 빈 페트병", "플라스틱", "images/petbottle_none_cola.png"));
 		itemTemplates.add(new Item("유리병", "유리", "images/glassbottle_sink_none.png"));
+		itemTemplates.add(new Item("유리컵", "유리", "images/glassbottle_cup.png"));
 		itemTemplates.add(new Item("종이", "종이", "images/paper.png"));
+		itemTemplates.add(new Item("포스트잇", "종이", "images/paper_2.png"));
+		itemTemplates.add(new Item("신문지", "종이", "images/paper_3.png"));
 
 		List<Bin> bins = new ArrayList<>();
 		bins.add(new Bin("플라스틱", "images/plastic_bin.png"));
@@ -43,10 +48,15 @@ public class LevelManager {
 		// 레벨2 데이터
 		List<Item> itemTemplates = new ArrayList<>();
 		itemTemplates.add(new Item("배달음식 용기", "플라스틱", "images/plastic_bowl.png"));
+		itemTemplates.add(new Item("물티슈 뚜껑", "플라스틱", "images/plastic_water.png"));
+		itemTemplates.add(new Item("배달음식 용기", "플라스틱", "images/plastic_squre.png"));
 		itemTemplates.add(new Item("빈 페트병", "플라스틱", "images/petbottle_black.png"));
 		itemTemplates.add(new Item("콜라 빈 페트병", "플라스틱", "images/petbottle_none_cola.png"));
 		itemTemplates.add(new Item("유리병", "유리", "images/glassbottle_sink_none.png"));
+		itemTemplates.add(new Item("유리컵", "유리", "images/glassbottle_cup.png"));
 		itemTemplates.add(new Item("종이", "종이", "images/paper.png"));
+		itemTemplates.add(new Item("포스트잇", "종이", "images/paper_2.png"));
+		itemTemplates.add(new Item("신문지", "종이", "images/paper_3.png"));
 		itemTemplates.add(new Item("휴지", "일반", "images/tissue.png"));
 
 		List<Bin> bins = new ArrayList<>();
@@ -62,11 +72,17 @@ public class LevelManager {
 		// 레벨3 데이터
 		List<Item> itemTemplates = new ArrayList<>();
 		itemTemplates.add(new Item("빈 페트병", "페트", "images/petbottle_black.png"));
+		//itemTemplates.add(new Item("물티슈 뚜껑", "플라스틱", "images/plastic_water.png"));
+		//itemTemplates.add(new Item("배달음식 용기", "플라스틱", "images/plastic_squre.png"));
 		itemTemplates.add(new ComplexItem("라벨이 있는 페트병", "페트#cutter#비닐", "images/petbottle_cola_label.png", new Item("페트병", "페트", "images/petbottle_none_cola.png"), new Item("라벨", "비닐", "images/lable_vinyl.png"), "cutter"));
 		itemTemplates.add(new Item("유리병", "유리", "images/glassbottle_sink_none.png"));
 		itemTemplates.add(new ComplexItem("음료수가 있는 페트병", "페트#sink#", "images/petbottle_sink.png", new Item("페트병", "페트", "images/petbottle_sink_none.png"), "sink"));
-
+		itemTemplates.add(new Item("지퍼팩", "비닐", "images/vinyl.png"));
+		itemTemplates.add(new Item("비닐봉지", "비닐", "images/vinyl_2.png"));
+		itemTemplates.add(new Item("유리컵", "유리", "images/glassbottle_cup.png"));
+		
 		List<Bin> bins = new ArrayList<>();
+		//bins.add(new Bin("플라스틱", "images/plastic_bin.png"));
 		bins.add(new Bin("페트", "images/pet_bin.png"));
 		bins.add(new Bin("유리", "images/glass_bin.png"));
 		bins.add(new Bin("비닐", "images/vinyl_bin.png"));
